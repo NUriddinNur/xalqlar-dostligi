0d6e39caec5db9f8efe44901bbcf904c29b22626
NurPro <nurik-31@mail.ru>
2022-09-26 09:06:43 +0500

    finish7

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ const router = Router()
 import userController  from '../controllers/userController.js'
 import authMiddlewares from '../middleware/authMiddleware.js'
 
+
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.post('/login/admin', userController.loginAdmin)
